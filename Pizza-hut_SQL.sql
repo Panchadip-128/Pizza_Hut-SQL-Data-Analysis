768002e1ddc418aa6f49f6240dea12e4c544cdb0
Panchadip <165953910+Panchadip-128@users.noreply.github.com>
2024-06-29 20:24:34 +0530

    Add files via upload

---
+++ Pizza-hut_SQL.sql
@@ -0,0 +1,19 @@
+SELECT 
+    category,
+    name,
+    revenue,
+    RANK() OVER (PARTITION BY category ORDER BY revenue DESC) AS rn
+FROM (
+    SELECT 
+        pt.category,
+        pt.name,
+        SUM(od.quantity * p.price) AS revenue
+    FROM 
+        pizza_types pt
+    JOIN 
+        pizzas p ON pt.pizza_type_id = p.pizza_type_id
+    JOIN 
+        order_details od ON od.pizza_id = p.pizza_id
+    GROUP BY 
+        pt.category, pt.name
+) AS a;
